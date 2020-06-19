f9ca5ec348e427674b72e1de8e9a6519bc6b9b2e
Halkidiki <mazurkiewiczpw@gmail.com>
2020-06-20 00:16:06 +0200

    Add MongoDB

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,10 @@ const fetch = require("node-fetch");
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require('mongoose');
 require("./services/passport");
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Server Settings
 const app = express();
@@ -21,6 +23,7 @@ app.listen(8000, function () {
     "Smart Light Mi listening on port 8000! \n You can close it by click Ctrl + c"
   );
 });
+
 
 //Objects:
 
