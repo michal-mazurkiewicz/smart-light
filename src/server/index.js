5b9a097c6daa2112db74c8c3377f21c11212749e
Halkidiki <mazurkiewiczpw@gmail.com>
2020-06-23 23:45:45 +0200

    Add PID Controller

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Controller = require("node-pid-controller");
 require("./models/User");
 require("./services/passport");
 
@@ -28,6 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/controlRoutes")(app);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,11 +44,21 @@ app.listen(8000, function () {
 
 //Objects:
 
-let results = [];
+users = [];
+
+user = {
+  id: "userId",
+  deviceList: [{ device: "ip", currentSettings: "currentPower" }],
+};
+
+let sensors = [{ ip: "192.168.1.14", meassures: [] }],
+  results = [];
 
 sensorData = {
   lumminance: "",
 };
+
+let ctr = new Controller(0.25, 0.01, 0.01, 1);
 
 //Declare Endpoints:
 
@@ -69,8 +81,26 @@ app.get("/getValue", function (req, res) {
 //Post:
 app.post("/sensor", function (req, res) {
   console.log("Incomming POST request: ", req.body);
-  const { lumminance } = req.body;
-  results.unshift(lumminance);
+  const { illumminance } = req.body;
+  let currSensor = sensors.find((sensor) => sensor.ip === req.body.sensor);
+  currSensor.meassures.unshift(illumminance);
+
+  if (currSensor.meassures.length > 15) {
+    let sorted = currSensor.meassures.sort((a, b) => a - b);
+    console.log("SORTED: ", sorted);
+    let sum = sorted.slice(5, 15).reduce((a, b) => a + b, 0)
+    let avg = (sum / 10) || 0;
+
+
+    console.log("AVERAGE: ", avg);
+    ctr.setTarget(30);
+
+    let output = illumminance;
+    let input = ctr.update(output);
+    console.log("CORRECTION", input);
+
+    currSensor.meassures.splice(0, 15);
+  }
   res.status(200).send("OK");
 });
 
@@ -98,6 +128,22 @@ const setColor = async (url = "", data) => {
   }
 };
 
+const setPower = async (url = "", data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const status = await response;
+    console.log("POST REQUEST /COLOR: ", status.statusText);
+  } catch (error) {
+    console.log("Change Color Error: ", error);
+  }
+};
+
 function getValue() {
   const values = results.slice(0, 9);
   let value = 0;
@@ -107,3 +153,7 @@ function getValue() {
   const result = value / 10;
   return { value: result };
 }
+
+//2 minuty collecting
+//20% najniższych liczb
+//Średnią albo co kilka minut najwyższą wartość.
